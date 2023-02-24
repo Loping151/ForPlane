a93e3b879b6af49ad6c6811d32480f4f198a97f9
Loping151 <wangkailing151@gmail.com>
2023-02-25 00:57:13 +0800

    scripts

---
+++ plenoxels/configs/final/endonerf/maskexps/exps.sh
@@ -0,0 +1,8 @@
+PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_direct_mask.py
+PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS.py --expname endo_hybrid_maskIS_1_100 --isg_step 1 --ist_step 100
+PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS.py --expname endo_hybrid_maskIS_1_300 --isg_step 1 --ist_step 300
+PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS.py --expname endo_hybrid_maskIS_1_500 --isg_step 1 --ist_step 500
+PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS.py --expname endo_hybrid_maskIS_1_1000 --isg_step 1 --ist_step 1000
+PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS.py --expname endo_hybrid_maskIS_1_1500 --isg_step 1 --ist_step 1500
+PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS.py --expname endo_hybrid_maskIS_1_2000 --isg_step 1 --ist_step 2000
+
