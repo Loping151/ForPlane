777a768fcf1e570fe39e7c917ff72585fa01d516
Loping151 <wangkailing151@gmail.com>
2023-02-25 19:46:34 +0800

    add echo

--- clear_weights.sh
+++ clear_weights.sh
@@ -6,5 +6,6 @@ folder_path="data"
 # 删除文件夹下所有以'.pt'结尾的文件
 find "$folder_path" -type f -name '*.pt' -delete
 
-echo "deleted'.pt'files"
+echo "deleted '.pt' files"
+echo "This is for recalculate isg and ist weight"
 
