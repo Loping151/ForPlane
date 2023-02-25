166d7da5f71ccfc5146b08054276f270c645dc85
Loping151 <wangkailing151@gmail.com>
2023-02-25 17:19:55 +0800

    fix file name

--- plenoxels/configs/final/endonerf/maskexps/exps.sh
+++ plenoxels/configs/final/endonerf/maskexps/exps.sh
@@ -1,11 +1,11 @@
 # cutting_tissues_twice
 
-PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_direct_mask_cutting.py 'logdir'='./logs/endo_hybrid_direct_mask/cutting'
-PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_100' 'isg_step'=1 'ist_step'=100 'logdir'='./logs/endo_hybrid_maskIS/cutting'
-PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_300' 'isg_step'=1 'ist_step'=300 'logdir'='./logs/endo_hybrid_maskIS/cutting'
-PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_500' 'isg_step'=1 'ist_step'=500 'logdir'='./logs/endo_hybrid_maskIS/cutting'
-PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_1000' 'isg_step'=1 'ist_step'=1000 'logdir'='./logs/endo_hybrid_maskIS/cutting'
-PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_1500' 'isg_step'=1 'ist_step'=1500 'logdir'='./logs/endo_hybrid_maskIS/cutting'
+# PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_direct_mask_cutting.py 'logdir'='./logs/endo_hybrid_direct_mask/cutting'
+# PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_100' 'isg_step'=1 'ist_step'=100 'logdir'='./logs/endo_hybrid_maskIS/cutting'
+# PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_300' 'isg_step'=1 'ist_step'=300 'logdir'='./logs/endo_hybrid_maskIS/cutting'
+# PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_500' 'isg_step'=1 'ist_step'=500 'logdir'='./logs/endo_hybrid_maskIS/cutting'
+# PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_1000' 'isg_step'=1 'ist_step'=1000 'logdir'='./logs/endo_hybrid_maskIS/cutting'
+# PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_1500' 'isg_step'=1 'ist_step'=1500 'logdir'='./logs/endo_hybrid_maskIS/cutting'
 PYTHONPATH='.' python plenoxels/main.py --config-path plenoxels/configs/final/endonerf/maskexps/endo_hybrid_maskIS_cutting.py 'expname'='endo_hybrid_maskIS_1_2000' 'isg_step'=1 'ist_step'=2000 'logdir'='./logs/endo_hybrid_maskIS/cutting'
 
 # pulling_soft_tissues
