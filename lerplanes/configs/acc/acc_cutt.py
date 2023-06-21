18e2a0ebfd7a959f0d0f033e6ec52bb86ace6868
chensjtu <ycyangchen@sjtu.edu.cn>
2023-06-21 19:20:05 +0800

    acc

--- lerplanes/configs/acc/acc_cutt.py
+++ lerplanes/configs/acc/acc_cutt.py
@@ -2,7 +2,7 @@ config = {
     'description': '',
 
     'expname': 'acc_cutt',
-    'logdir': './logs/acc',
+    'logdir': './logs/standard',
     'device': 'cuda:0',
 
     'data_downsample': 1.0,
@@ -12,7 +12,7 @@ config = {
     'ndc_far': 2.0,
     'isg': True,
     'isg_step': 0,
-    'ist_step': 180,
+    'ist_step': 180*2,
     'keyframes': False,
     'scene_bbox': [[-1.0, -1.0, -1.0], [1.0, 1.0, 0.1]],
     'maskIS': True,
@@ -21,12 +21,12 @@ config = {
     'bg_color': 0,
     'depth_type': 'depth',
     # Optimization settings
-    'num_steps': 1800,
-    'batch_size': 32768,
+    'num_steps': 1800*2,
+    'batch_size': 32768//2,
     'scheduler_type': 'warmup_cosine',
     'optim_type': 'adam',
     'lr': 0.01,
-    "eval_batch_size": 65536,
+    "eval_batch_size": 65536//2,
 
     # acc
     'occ_grid_reso': 64,
@@ -47,11 +47,11 @@ config = {
     'time_smoothness_weight_proposal_net': 0.0001,
     'depth_huber_weight': 0.05,
     'depth_huber_weight_proposal_net': 0.05,
-    'step_iter': 900,
+    'step_iter': 900*2,
 
     # Training settings
-    'valid_every': 1800,
-    'save_every': 1800,
+    'valid_every': 1800*2,
+    'save_every': 1800*2,
     'save_outputs': True,
     'train_fp16': True,
 
